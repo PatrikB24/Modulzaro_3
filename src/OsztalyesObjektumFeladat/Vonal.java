397266b15b36f278250cc34eccb8761f785252b1
PatrikB <bekpatrik24@gmail.com>
2024-08-03 12:02:06 +0200

    SetterekMetodusok

--- src/OsztalyesObjektumFeladat/Vonal.java
+++ src/OsztalyesObjektumFeladat/Vonal.java
@@ -1,6 +1,4 @@
-
 package OsztalyesObjektumFeladat;
-
 
 public class Vonal {
 
@@ -16,8 +14,8 @@ public class Vonal {
     private int hossz;
     private int eltolas;
     private Szinek szin;
-    private Stilusok stilus;    
- 
+    private Stilusok stilus;
+
 //konstruktorok    
     public Vonal(int hossz) {
         this(hossz, 0, Szinek.PIROS, Stilusok.SZIMPLA);
@@ -33,7 +31,7 @@ public class Vonal {
 
     public Vonal(int hossz, int eltolas, Szinek szin, Stilusok stilus) {
         if (hossz < 1) {
-        System.out.println("Hiba: A hossz nem lehet kisebb, mint 1!");
+            System.out.println("Hiba: A hossz nem lehet kisebb, mint 1!");
             this.hossz = 1;
         } else {
             this.hossz = hossz;
@@ -47,7 +45,45 @@ public class Vonal {
         this.szin = szin;
         this.stilus = stilus;
     }
-    
-    
-}
 
+//setterek
+    public void setEltolas(int eltolas) {
+        if (eltolas < 0) {
+            System.out.println("Hiba: Az eltolás nem lehet negatív! Nem történt változtatás.");
+        } else {
+            this.eltolas = eltolas;
+        }
+    }
+
+    public void setSzin(Szinek szin) {
+        this.szin = szin;
+    }
+
+    public void setStilus(Stilusok stilus) {
+        this.stilus = stilus;
+    }
+
+//metódusok
+    @Override
+    public String toString() {
+        return "Vonal{hossz=" + hossz + ", eltolás=" + eltolas + ", szin=" + szin + ", stilus=" + stilus + "}";
+    }
+
+    public String rajz() {
+        String abra = "";
+        for (int i = 0; i < eltolas; i++) {
+            abra += " ";
+        }
+        for (int i = 0; i < hossz; i++) {
+            if (stilus == Stilusok.SZIMPLA) {
+                abra += "_";
+            } else if (stilus == Stilusok.DUPLA) {
+                abra += "=";
+            } else if (stilus == Stilusok.PONT) {
+                abra += ".";
+            }
+        }
+        return abra;
+
+    }
+}
